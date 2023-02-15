f8c6f92a1b8c3e815858f3949520246484adb529
AASports89 <aavakyan16@students.kgi.edu>
2023-02-14 16:30:40 -0800

    formmatting

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -35,54 +35,53 @@ function Clean_Claim_Calc() {
   }
 
   return (
-<div data-aos="slide-right" className='row justify-content-center' id="calc-main">
+
+    <div data-aos="slide-right" className='card-body'>
     
-    <div className='col-5 justify-content-center' id="title-calc">
         <h1 id="calc-title" className='card-title'>
             View Revenue Opportunity Amount  <i id="rev" class="fa-solid fa-magnifying-glass-dollar"></i>
         </h1>
+        <div className='row justify-content-center' id="calc-main">
+        <div className='col-5 justify-content-center' id="title-calc">
         <h5 id="calc-subtitle" className='card-subtitle'>
             <ul>
             <span id="badge-1" data-aos="zoom-in-right" className="badge">
                 <img id="icon-ligo" src="https://res.cloudinary.com/dhqsixgmo/image/upload/v1675882601/ligo_logo_k9avxm.svg" alt="ligo icon"></img>
             </span> 
-            Enter the average volume of billing encounters and/or claims your laboratory processes per month or year.
+            Enter the average volume of billing encounters and/or claims your laboratory processes montly or yearly.
             </ul>
             <ul>
             <span id="badge-1" data-aos="zoom-in-right" className="badge">
                 <img id="icon-ligo" src="https://res.cloudinary.com/dhqsixgmo/image/upload/v1675882601/ligo_logo_k9avxm.svg" alt="ligo icon"></img>
             </span>
-                The calculated number represents the potential revenue opportunity with the integration of LigoLab™ into laboratory operations.
+                The calculated number represents the potential revenue increase w/ the integration of LigoLab™.
             </ul>
             
         </h5>
     </div>
      
      <div className='col-2' id="calc-form">
-     <form className='form-group'>
-        <div className='form-row-mb-5' id='calc-disp'>
+        <form className='form-group'>
+
+          <div className='form-row-mb-5' id='calc-disp'>
             <input className='form-control' type="text" id="result" placeholder='Rev. Op.' value={'$' + currentSum}  readOnly />   
-        </div>
-        <div className='form-row-mb-3' id="calc-input">
+          </div>
+          <div className='form-row-mb-3' id="calc-input">
             <input className='form-control' type="text" id="num" placeholder="Enter vol..." />
-        </div>
-        <div className='form-row-mb-3' id="calc-btns">
+          </div>
+          <div className='form-row-mb-3' id="calc-btns">
             <button id="calc" className='btn btn-light' onClick={Add}>Calculate</button>
             <button id="clear" className='btn btn-light' onClick={Clear}>Reset</button>
-        </div>
-        <div className='col-12'>
-            <ol id="disclaimer">
-            <i id="ast" className="fa-solid fa-asterisk"></i>Estimates are based on average statistics & automated calculations, in no way are exact, precise, or guaranteed.
-            </ol>
-        </div>
-       
-
-    </form>
-
+          </div>
+          <div className='col-12'>
+              <ol id="disclaimer">
+                <i id="ast" className="fa-solid fa-asterisk"></i>Estimates are based on average statistics & automated calculations, in no way are exact, precise, or guaranteed.
+              </ol>
+          </div>
+        </form>
+      </div>
      </div>
        
-     
-        
     </div>
   );
 }
