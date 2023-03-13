bb05483432e44b14a49bb7d4e657f8a9b67e98f6
AASports89 <aavakyan16@students.kgi.edu>
2023-03-13 02:18:56 -0700

    formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,15 +24,11 @@ export default function Header() {
 
     return (
 		<div className="container-fluid bg-light mt-5" id="navbar-div">
-			<nav data-aos="slide-down" id="navbar" className="navbar">
-				
-				<a className="home-style navbar-brand" id="main-logo-link" href="www.ligolab.com" onClick={() => { window.scrollTo({ top: 0, behavior: "smooth" }); }}>
-					<img id="logo" className="img-fluid" src="https://res.cloudinary.com/dhqsixgmo/image/upload/v1678591972/LIGOLAB_FULL_LOGO_mbj5er.svg" alt="ligolab"></img>
-				</a>
-
-			</nav>
 
 			<nav data-aos="slide-down" class="navbar navbar-expand-lg navbar-light bg-light" id="nav-cont">
+				<a data-aos="slide-down" className="home-style navbar-brand" id="main-logo-link" href="www.ligolab.com" onClick={() => { window.scrollTo({ top: 0, behavior: "smooth" }); }}>
+					<img id="logo" className="img-fluid" src="https://res.cloudinary.com/dhqsixgmo/image/upload/v1678591972/LIGOLAB_FULL_LOGO_mbj5er.svg" alt="ligolab"></img>
+				</a>
 				<div className="navbar-nav" data-aos="slide-down" id="nav-links">
 					<a onClick={() => SmoothScrollTo("contact-form")} href="#contact-form" id="nav-link" className="btn-style nav-item nav-link">SCHEDULE FREE CONSULTATION</a>
 					<a onClick={() => SmoothScrollTo("revenue_gaps")} href="#revenue_gaps" id="nav-link" className="btn-style nav-item nav-link">A SINGLE-SOURCE OF TRUTH</a>
